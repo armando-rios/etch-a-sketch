c4d83853e489dea172afddf325daba546206b638
Armando Rios <armandorios.dev@proton.me>
2024-10-08 04:46:38 -0700

    basic program logic created

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-const $ = value => document.querySelector(value)
+const $ = (value) => document.querySelector(value);
+const $$ = (value) => document.querySelectorAll(value);
 
-const container = $(".container")
+const container = $(".container");
 
+const rows = 10 ** 2;
 
+for (let index = 0; index < rows; index++) {
+    container.innerHTML += `<div class="paint"></div>`;
+}
+
+const paint = $$(".paint");
+
+paint.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        element.classList.add("paintColor");
+    });
+    const resetBtn = $("#reset");
+    resetBtn.addEventListener("click", () => {
+        element.classList.remove("paintColor");
+    });
+});
