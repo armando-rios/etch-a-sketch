0f9794c572ca02bb6696ddd2ec1dafc97e2fb837
Armando Rios <armandorios.dev@proton.me>
2024-10-11 20:09:46 -0700

    Refactored code to function and fix reset button

--- main.js
+++ main.js
@@ -1,66 +1,46 @@
-const $ = (value) => document.querySelector(value);
-const $$ = (value) => document.querySelectorAll(value);
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
 
 const container = $(".container");
 const rowInput = $("#rows");
 const rowSubmit = $("#rows-submit");
 const changeColor = $("#change-color");
 const colorInput = $("#colorInput");
-
+const resetBtn = $("#reset")
 const containerSize = 600;
-
 let rows;
 
-// funcion para cambiar el color
-colorInput.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const paint = $$(".paint");
-
-  paint.forEach((element) => {
-    element.addEventListener("mouseover", () => {
-      // element.classList.add("paintColor");
-      element.style.background = changeColor.value;
-    });
+const addPaintListeners = () => {
+  $$(".paint").forEach((element) => {
+    element.onmouseover = () => (element.style.background = changeColor.value);
   });
-  console.log(changeColor.value);
-});
+};
 
-// funcion para obtener el numero de filas
-rowSubmit.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  container.innerHTML = "";
-
-  rows = parseInt(rowInput.value);
-  if (rows > 100) {
-    return alert("No puede usar mas de 100");
-  }
-
+const dibujarCuadros = (rows) => {
+  if (rows > 100 || rows < 1)
+    return alert("No puede usar mas de 100 o menos de 1");
+    container.innerHTML = ""
   const totalSquares = rows ** 2;
+  const squareSize = `${containerSize / rows}px`;
 
-  const squareSize = containerSize / rows;
-
-  for (let index = 0; index < totalSquares; index++) {
+  for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement("div");
     square.classList.add("paint");
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
+    square.style.width = squareSize;
+    square.style.height = squareSize;
     container.appendChild(square);
   }
-  const paint = $$(".paint");
+  addPaintListeners();
+};
 
-  paint.forEach((element) => {
-    element.addEventListener("mouseover", () => {
-      // element.classList.add("paintColor");
-      element.style.background = "red";
-    });
-  });
-});
+rowSubmit.onsubmit = (e) => {
+  e.preventDefault();
+  dibujarCuadros(parseInt(rowInput.value));
+};
 
-const resetBtn = $("#reset");
-resetBtn.addEventListener("click", () => {
-  const paint = $$(".paint");
-  paint.forEach((element) => {
-    element.classList.remove("paintColor");
-  });
-});
+colorInput.onsubmit = (e) => e.preventDefault();
+
+resetBtn.onclick = () =>
+  $$(".paint").forEach((el) => (el.style.background = ""));
+
+dibujarCuadros(10);
